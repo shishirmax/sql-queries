9391049941d175e484f9881ec77384c55c9bdc4b
Shishir <shishirmax@gmail.com>
2017-06-29 14:38:46 +0530

    query update

--- sql-over.sql
+++ sql-over.sql
@@ -7,20 +7,24 @@ Profit money
 
 insert into shipment
 values
+('7/19/2016','Item101',4,'$20'),
 ('7/21/2016','Item101',5,'$25'),
 ('7/1/2016','Product411',11,'$44'),
 ('7/1/2016','Product411',14,'$56')
 
 select * from shipment
 
-declare @TotalQtyShip int
-set @TotalQtyShip = SUM(qtyship) over(partition by invtid)
 select 
 	replace(CONVERT(VARCHAR(10), ShipDate, 101),'-','/') as ShipDate,	
 	InvtID,
 	QtyShip,
 	Profit,
-	SUM(qtyship) over(partition by invtid) as TotalQtyShip
+	SUM(qtyship) over(partition by invtid) as TotalQtyShip,
+	SUM(profit) over (partition by invtid) as TotalProfit
 from shipment
+where ShipDate > '07/01/2016'
 
-TotalProfit = TotalQtyShip * Profit
+--TotalProfit = TotalQtyShip * Profit
+
+--484+784
+--80+125
