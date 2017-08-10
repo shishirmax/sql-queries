4dbe2bae7f3251acdba204ca208b7374a3c3a48a
Shishir <shishirmax@gmail.com>
2017-08-10 16:49:51 +0530

    new sql queries

--- CountTotal.sql
+++ CountTotal.sql
@@ -3,6 +3,11 @@ id int identity(1,1),
 filename varchar(100)
 )
 
+create table filedata2(
+id int identity(1,1),
+filename2 varchar(100)
+)
+
 insert into filedata
 values
 ('file1'),
@@ -12,15 +17,40 @@ values
 ('file3'),
 ('file4')
 
+
+insert into filedata2
+values
+('file1'),
+('file1'),
+('file1'),
+('file2'),
+('file2'),
+('file3'),
+('file3'),
+('file4'),
+('file4')
+
 select * from filedata
+select * from filedata2
 
 select 
 filename,
 count(filename)as filecount,
-totalCount = (select count(distinct filename) from filedata)
+totalCount = (
+				select count(distinct filename) from filedata
+				join filedata2
+				on filedata.filename = filedata2.filename2
+			 )
 from filedata
 group by filename
 
 
 select count(*) from
-(select count(distinct filename) from filedata) T
+(
+select count(distinct filename) from filedata
+join filedata2
+on filedata.filename = filedata2.filename2
+) T
+
+select count(distinct filename) from filedata
+select count(distinct filename2) from filedata2
