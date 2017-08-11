08b8ac1248a7f33329a43a7ab0d6c6cb65e3b868
Shishir <shishirmax@gmail.com>
2017-08-11 16:42:46 +0530

    totalcount and uniquecount

--- CountTotal.sql
+++ CountTotal.sql
@@ -60,3 +60,38 @@ select count(distinct filename2) from filedata2
 
 select count(filename)as filecount_TableA from filedata
 select count(filename2)as filecount_TableB from filedata2
+
+select count(distinct filename) as totalcount
+from filedata
+
+
+select 
+A.filename,
+count(A.filename)as filecount_TableA,
+totalCount = (
+				select count(distinct filename) from filedata
+			 )
+from filedata A
+group by A.filename
+
+
+SELECT filename, COUNT( * ) AS filecount_TableA
+FROM filedata
+GROUP BY filename
+
+SELECT filename2, COUNT( * ) AS filecount_TableB
+FROM filedata2
+GROUP BY filename2
+
+SELECT filename, filename2, filecount_TableA, filecount_TableB, ISNULL( filecount_TableA, 0 ) + ISNULL( filecount_TableB, 0 ) AS totalCount,
+    COUNT(*) OVER() AS UniqueFileCount
+FROM
+        ( SELECT filename, COUNT( * ) AS filecount_TableA
+        FROM filedata
+        GROUP BY filename ) AS A
+    --FULL OUTER JOIN
+	inner join
+            ( SELECT filename2, COUNT( * ) AS filecount_TableB
+            FROM filedata2
+            GROUP BY filename2 ) AS B
+        ON A.filename = B.filename2
