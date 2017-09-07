7d00a39a7c0a11660fa2e370fe9db5c5b1c2c04c
Shishir <shishirmax@gmail.com>
2017-09-07 15:44:56 +0530

    Solution to HackerRank SQL problems

---
+++ HackerRank-Solutions/sol_1.sql
@@ -0,0 +1,32 @@
+select Country.Continent,AVG(CITY.Population)
+from CITY
+join Country
+on CITY.CountryCode = Country.Code
+group by country.continent
+
+--The Report
+select 
+	CASE
+		when grades.grade<8
+			Then NULL
+		ELSE 
+			student.name
+	END As Name,student.marks,grades.grade
+from student
+join grades
+on student.marks between grades.min_mark and grades.max_mark
+order by grades.grade desc,student.name
+
+--Top Competitors
+select h.hacker_id, h.name
+from submissions s
+inner join challenges c
+on s.challenge_id = c.challenge_id
+inner join difficulty d
+on c.difficulty_level = d.difficulty_level 
+inner join hackers h
+on s.hacker_id = h.hacker_id
+where s.score = d.score 
+group by h.hacker_id, h.name
+having count(h.hacker_id) > 1
+order by count(h.hacker_id) desc, h.hacker_id asc;
