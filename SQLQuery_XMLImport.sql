edb1a239b9510140ee56a47300a4687b55ba26c8
Shishir <shishirmax@gmail.com>
2017-09-14 15:39:26 +0530

    import xml file

--- SQLQuery_XMLImport.sql
+++ SQLQuery_XMLImport.sql
@@ -1,16 +1,21 @@
-create table tblXMLData(
-id int identity,
-XMLData xml,
-loadedDate datetime)
+--create table tblXMLData(
+--id int identity,
+--XMLData xml,
+--loadedDate datetime)
 
 insert into tblXMLData(XMLData,loadedDate)
 select CONVERT(xml,BulkColumn) As BulkColumn, GETDATE()
-from openrowset(BULK 'D:\Edina\DataExtract\Generated Error\04_607123.xml',SINGLE_BLOB)As x
+from openrowset(BULK 'D:\Edina\DataExtract\Generated Error\13_607265.xml',SINGLE_BLOB)As x
 
 select * from tblXMLData
 
+select ASCII('‘')
+select ASCII('¼')
+select ASCII('°')
+select ASCII('’')
+select ASCII('”')
 --truncate table tblxmlData
-drop table tblxmlData
+--drop table tblxmlData
 
 --declare @xml as xml,@hDoc as int, @sql nvarchar(max)
 --select @xml = xmldata from tblXMLData
@@ -18,25 +23,34 @@ drop table tblxmlData
 
 --exec sp_xml_removedocument @hDoc
 
-alter procedure sp_ImportXMLFile
-@filename varchar(max)
-as
-begin
-declare @query varchar(max)
-set @query = 
-'insert into tblXMLData(XMLData,loadedDate,'+@filename+')
-select CONVERT(xml,BulkColumn) As BulkColumn, GETDATE()
-from openrowset(BULK ''D:\Edina\DataExtract\Generated Error\'+@filename+'''+,SINGLE_BLOB)As x'
-execute (@query)
-end
 
-sp_ImportXMLFile '01_605836.xml'
+--create table tblXMLDataWithFileName(
+--id int identity(1,1),
+--XMLData xml,
+--loadedDate datetime,
+--XMLFilename varchar(max))
+
+--select * from tblXMLDataWithFileName
+
+--alter procedure sp_ImportXMLFileWithFilename
+--@filename varchar(max)
+--as
+--begin
+--declare @query varchar(max)
+--set @query = 
+--N'insert into tblXMLDataWithFileName(XMLData,loadedDate,'''+@filename+''')
+--select CONVERT(xml,BulkColumn) As BulkColumn, GETDATE()
+--from openrowset(BULK ''D:\Edina\DataExtract\Generated Error\' + @filename + ''',SINGLE_BLOB)As x'
+--execute (@query)
+--end
+
+--sp_ImportXMLFileWithFilename 'testXml.xml'
 
 
-declare @XMLDoc XML
-declare @XMLDocId int
-set @XMLDoc = (select XMLData from tblXMLData where id = 1)
+--declare @XMLDoc XML
+--declare @XMLDocId int
+--set @XMLDoc = (select XMLData from tblXMLData where id = 14)
 
-exec sys.sp_xml_preparedocument @XMLDocId output, @XMLDoc
+--exec sys.sp_xml_preparedocument @XMLDocId output, @XMLDoc
 
-select * from openxml(@XMLDocId,'/us.mn.state.mdor.ecrv.extract.form.EcrvForm/') ox;
+--select * from openxml(@XMLDocId,'/us.mn.state.mdor.ecrv.extract.form.EcrvForm/') ox;
