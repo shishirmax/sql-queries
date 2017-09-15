ad95fa1d292217c8df301cfa571dd2d2e1aac127
Shishir <shishirmax@gmail.com>
2017-09-15 15:24:04 +0530

    sample test query

---
+++ just_for_test_edina.sql
@@ -0,0 +1,27 @@
+select * from sys.tables
+
+select * from tblEmailResults
+select * from tblWebsiteData
+select * from tblSales
+
+select * from tblDynamicsCRM_POC
+select * from tblPropertyForm
+
+select * from xmlimport
+select * from xmltab
+select * from xtab
+
+sp_helptext sp_xml_preparedocument 
+
+select * from tblHeaderForm
+select * from tblbuyersForm
+select * from tblSellersForm
+select * from tblPropertyForm
+select * from tblSalesAgreementForm
+select * from tblSupplementaryForm
+
+
+sp_helptext fn_diagramobjects
+
+select convert(date,getdate(),101)
+exec dbo.sp_MSgetdbversion
