6dc6d6aafa4a0697ee455320ec5a7e69b8589e0c
Shishir <shishirmax@gmail.com>
2017-09-19 18:49:33 +0530

    Query Updated

--- SQLQuery-Analysis.sql
+++ SQLQuery-Analysis.sql
@@ -1,13 +1,24 @@
-select count(1) from tblWebsiteData --(1689734)
-select COUNT(1) from tblSales(NOLOCK) --(266104)
-select count(1) from tblEmailResults --(3339200)
+select count(1) from tblWebsiteData --(844867)
+select COUNT(1) from tblSales(NOLOCK) --(133052)
+select count(1) from tblEmailResults --(1669600)
+
+
+select count(1) from sales
+
+truncate table tblSales
+
+insert into tblSales
+(
 
 sp_help tblWebsiteData
 sp_help tblSales
 sp_help tblEmailResults
 
-select ListingId,count(*) from tblWebsiteData
-group by ListingId
+select WebsiteUserId,count(*) from tblWebsiteData
+where WebsiteUserId IS NULL
+group by WebsiteUserId
+
+select count(WebsiteUserId) from tblWebsiteData
 
 select RecordIDs,count(*) from tblSales
 group by RecordIDs
@@ -24,7 +35,7 @@ select max(cast(CreatedDate as date)) from tblWebsiteData
 
 select count(*) as tblRowCount,YEAR(CreatedDate) as tblYear,MONTH(CreatedDate) as tblMonth from tblWebsiteData
 group by YEAR(CreatedDate),MONTH(CreatedDate)
-order by YEAR(CreatedDate)
+order by YEAR(CreatedDate),MONTH(CreatedDate)
 
 2016: 536875
 2017: 307992
@@ -39,6 +50,18 @@ select  max(LEN(PhoneNumber)) from tblWebsiteData
 
 select LEN('a412edee7d9631e69aabfbdc917038b0efa7ddb6')
 --YYYY-MM-DDThh:mm:ss[.mmm]
+sp_help tblEmailResults
+select * from tblEmailResults
+select IsActive,count(*) as totalCount from tblEmailResults
+where IsActive is NULL
+group by IsActive
+
+select LastName,count(*) as totalCount from tblEmailResults
+where LastName is NULL
+group by LastName
+
+select  max(LEN(MdmContactId)) from tblEmailResults
+
 
 select min(cast(ResultDate as date)) from tblEmailResults
 select max(cast(ResultDate as date)) from tblEmailResults
@@ -47,10 +70,21 @@ select max(cast(ResultDate as date)) from tblEmailResults
 
 select count(*) as tblRowCount,YEAR(ResultDate) as tblYear,MONTH(ResultDate) as tblMonth from tblEmailResults
 group by YEAR(ResultDate),MONTH(ResultDate)
-order by YEAR(ResultDate)
+order by YEAR(ResultDate),MONTH(ResultDate)
+
+--tblSales Analysis
 
 sp_help tblSales
+
+select * from tblSales
 select RECORDING_DATE,sale_date from tblSales
+
+select Suffix,count(*) as totalCount from tblSales
+where Suffix is NULL
+group by Suffix
+
+
+
 
 select count(*) as tblRowCount,YEAR(sale_date) as tblYear,MONTH(sale_date) as tblMonth from tblSales
 group by YEAR(sale_date),MONTH(sale_date)
