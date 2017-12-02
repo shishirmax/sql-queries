6f663e51f03a97e7298d67a36018066966da6295
Shishir <shishirmax@gmail.com>
2017-12-02 12:46:04 +0530

    update BCP Script

--- bcp_script.sql
+++ bcp_script.sql
@@ -1,5 +1,9 @@
+**Create Table before importing data using BCP Script**
 CREATE TABLE websitedata(ListingId NVARCHAR(MAX),CreatedDate NVARCHAR(MAX),Rating NVARCHAR(MAX),Type NVARCHAR(MAX),WebsiteUserId NVARCHAR(MAX),LastName NVARCHAR(MAX),FirstName NVARCHAR(MAX),Email NVARCHAR(MAX),PhoneNumber NVARCHAR(MAX))
 
-bcp dbo.websitedata in D:\EdinaRealityWorbixAnalytics\DataExtract\FTPFiles\Files_18092017\website-data.txt -S contata.database.windows.net -d Edina -U contata.admin -P C@ntata123 -a 16384 -b 20000 -q -c -t"||"
 
-bcp dbo.tblHomeSpotter in D:\Edina\HomeSpotterFeed\29Nov17\edina_contata_sessions.csv -S tcp:contata.database.windows.net -d Edina_qa -U contata.admin@contata -P C@ntata123 -a 16384 -b 20000 -q -c -t","
+**BCP Script**
+
+bcp dbo.<tablename> in <file location in local folder> -S <server_name> -d <database_name> -U <username> -P <password> -a 16384 -b 20000 -q -c -t"||"
+
+bcp dbo.<tablename> in <file location in local folder> -S tcp:<server_name> -d <database_name> -U <servername>.<username> -P <password> -a 16384 -b 20000 -q -c -t","
