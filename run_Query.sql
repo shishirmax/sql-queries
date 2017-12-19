0d48610075f66f8ab2957b48ca09be3236a02a72
Shishir <shishirmax@gmail.com>
2017-12-19 16:20:20 +0530

    query updated

--- run_Query.sql
+++ run_Query.sql
@@ -21,24 +21,29 @@ SELECT COUNT(1) FROM homeSpotter.tblHomeSpotterHistory_FF
 -- MAXVALUE 2147483647
 -- CACHE 
 
-bcp homeSpotter.tblHomeSpotter_bcp in D:\Edina\HomeSpotterFeed\16Dec17\edina_contata_sessions_copy.csv -S tcp:contata.database.windows.net -d Edina_QA -U contata.admin@contata -P C@ntata123  -b 20000 -q -c -t","
+--HomeSpotter
+bcp homeSpotter.tblHomeSpotter_bcp in D:\Edina\HomeSpotterFeed\17Dec17\edina_contata_sessions.csv -S tcp:contata.database.windows.net -d Edina_QA -U contata.admin@contata -P C@ntata123  -b 20000 -q -c -t","
+
+--HomeSpotterHistory
+bcp homeSpotter.tblHomeSpotterHistory_bcp in D:\Edina\HomeSpotterFeed\HistoryData\contata_history-11-30-17_TestCopy.csv -S tcp:contata.database.windows.net -d Edina_QA -U contata.admin@contata -P C@ntata123  -b 20000 -q -c -t","
 
 
-
-
-SELECT * FROM homeSpotter.tblHomeSpotter_bcp WHERE [session_end_guess_utc] IS NOT NULL AND TRY_CAST([session_end_guess_utc] as datetime) IS NULL; 
+--SELECT * FROM homeSpotter.tblHomeSpotter_bcp WHERE [session_end_guess_utc] IS NOT NULL AND TRY_CAST([session_end_guess_utc] as datetime) IS NULL; 
 
 EXEC homeSpotter.usp_InsertHomeSpotter
 
 EXEC homeSpotter.usp_MergeHomeSpotter
 
-select * from logtaskControlFlow order by 1 desc --350
+select * from logtaskControlFlow order by 1 desc --365
 
+SELECT * FROM homeSpotter.tblHomeSpotter_bcp
 SELECT * FROM homeSpotter.tblHomeSpotter_FF
 SELECT * FROM homeSpotter.tblHomeSpotter_DT
-SELECT * FROM homeSpotter.tblHomeSpotter_AE
+SELECT * FROM homeSpotter.tblHomeSpotter_AE ORDER BY 1 DESC
 
-select * from logerror order by 1 desc --163
+
+
+select * from logerror order by 1 desc --176
 
 SELECT * FROM homeSpotter.DimAgent
 SELECT * FROM homeSpotter.DimDevice
@@ -47,19 +52,75 @@ SELECT * FROM homeSpotter.DimUser
 SELECT * FROM homeSpotter.FactHomeSpotter
 SELECT * FROM homeSpotter.FactHomeSpotterSummary
 
-TRUNCATE TABLE homeSpotter.DimAgent
-TRUNCATE TABLE homeSpotter.DimDevice
-TRUNCATE TABLE homeSpotter.DimSession
-TRUNCATE TABLE homeSpotter.DimUser
-TRUNCATE TABLE homeSpotter.FactHomeSpotter
-TRUNCATE TABLE homeSpotter.FactHomeSpotterSummary
+--TRUNCATE TABLE homeSpotter.DimAgent
+--TRUNCATE TABLE homeSpotter.DimDevice
+--TRUNCATE TABLE homeSpotter.DimSession
+--TRUNCATE TABLE homeSpotter.DimUser
+--TRUNCATE TABLE homeSpotter.FactHomeSpotter
+--TRUNCATE TABLE homeSpotter.FactHomeSpotterSummary
 
 
-(105 rows affected)
-UPDATE [homeSpotter].[tblHomeSpotter_FF] SET GoodToImport = 0, ErrorDescription = CONCAT(ErrorDescription,'| Error Column Name is [session_end_is_guess] ' + 'and value is : ('+ CAST([session_end_is_guess] as nvarchar(max))+')') WHERE [session_end_is_guess] IS NOT NULL AND TRY_CAST([session_end_is_guess] as int) IS NULL; 
+EXEC homeSpotter.usp_InsertHomeSpotterHistory
 
-(1 row affected)
-UPDATE [homeSpotter].[tblHomeSpotter_FF] SET GoodToImport = 0, ErrorDescription = CONCAT(ErrorDescription,'| Error Column Name is [session_end_guess_utc] ' + 'and value is : ('+ CAST([session_end_guess_utc] as nvarchar(max))+')') WHERE [session_end_guess_utc] IS NOT NULL AND TRY_CAST([session_end_guess_utc] as datetime) IS NULL; 
+EXEC homeSpotter.usp_MergeHomeSpotterHistory
 
-(64 rows affected)
-UPDATE [homeSpotter].[tblHomeSpotter_FF] SET GoodToImport = 0, ErrorDescription = CONCAT(ErrorDescription,'| Error Column Name is [ModifiedDate] ' + 'and value is : ('+ CAST([ModifiedDate] as nvarchar(max))+')') WHERE [ModifiedDate] IS NOT NULL AND TRY_CAST([ModifiedDate] as datetime) IS NULL; 
+SELECT * FROM homeSpotter.tblHomeSpotterHistory_bcp
+SELECT * FROM homeSpotter.tblHomeSpotterHistory_FF
+SELECT * FROM homeSpotter.tblHomeSpotterHistory_DT
+SELECT * FROM homeSpotter.tblHomeSpotterHistory_AE
+
+--TRUNCATE TABLE homeSpotter.tblHomeSpotterHistory_bcp
+--TRUNCATE TABLE homeSpotter.tblHomeSpotterHistory_FF
+--TRUNCATE TABLE homeSpotter.tblHomeSpotterHistory_DT
+--TRUNCATE TABLE homeSpotter.tblHomeSpotterHistory_AE
+
+--(105 rows affected)
+--UPDATE [homeSpotter].[tblHomeSpotter_FF] SET GoodToImport = 0, ErrorDescription = CONCAT(ErrorDescription,'| Error Column Name is [session_end_is_guess] ' + 'and value is : ('+ CAST([session_end_is_guess] as nvarchar(max))+')') WHERE [session_end_is_guess] IS NOT NULL AND TRY_CAST([session_end_is_guess] as int) IS NULL; 
+
+--(1 row affected)
+--UPDATE [homeSpotter].[tblHomeSpotter_FF] SET GoodToImport = 0, ErrorDescription = CONCAT(ErrorDescription,'| Error Column Name is [session_end_guess_utc] ' + 'and value is : ('+ CAST([session_end_guess_utc] as nvarchar(max))+')') WHERE [session_end_guess_utc] IS NOT NULL AND TRY_CAST([session_end_guess_utc] as datetime) IS NULL; 
+
+--(64 rows affected)
+--UPDATE [homeSpotter].[tblHomeSpotter_FF] SET GoodToImport = 0, ErrorDescription = CONCAT(ErrorDescription,'| Error Column Name is [ModifiedDate] ' + 'and value is : ('+ CAST([ModifiedDate] as nvarchar(max))+')') WHERE [ModifiedDate] IS NOT NULL AND TRY_CAST([ModifiedDate] as datetime) IS NULL; 
+
+--sp_helptext sp_RENAME
+--sp_RENAME 'homeSpotter.tblHomeSpotterHistory_DT.HS_DT_ID','HSHistoryId','COLUMN' --Done
+--sp_RENAME 'homeSpotter.tblHomeSpotterHistory_FF.HS_FF_ID','HSHistoryId','COLUMN' --Done
+--sp_RENAME 'homeSpotter.tblHomeSpotterHistory_AE.HS_AE_ID','HSHistoryId','COLUMN' --Done
+select * from sys.tables t
+where t.name = 'tblHomeSpotter_DT'
+
+DECLARE @ctr int;  
+DECLARE @temp AS TABLE
+		(
+			colname			NVARCHAR(255)
+			,datatype		NVARCHAR(255)
+			,datatypeLength INT
+			,rownum			INT
+		); 
+INSERT INTO @temp 
+SELECT  
+			c.name AS colname, 
+			CAST(u.name AS NVARCHAR(MAX))+     
+			CASE      
+				WHEN u.name like 'NVARCHAR'							THEN '('+CAST(c.max_length/2 AS NVARCHAR(MAX))+')'     
+				WHEN u.name like 'VARCHAR'							THEN '('+CAST(c.max_length AS NVARCHAR(MAX))+')'     
+				WHEN u.name like 'DECIMAL' OR u.name LIKE 'NUMERIC' THEN '('+ CAST(c.[precision] AS NVARCHAR(MAX))+',' + CAST(c.scale AS NVARCHAR(MAX)) +')'        else ''    
+			END AS datatype,
+			CASE
+				WHEN u.name LIKE 'NVARCHAR' THEN c.max_length/2
+				WHEN u.name LIKE 'VARCHAR' THEN c.max_length
+				WHEN u.name LIKE 'DECIMAL' OR u.name LIKE 'NUMERIC' THEN (c.[precision] + 1)
+			END AS datatypeLength, 
+			ROW_NUMBER() OVER (ORDER BY c.name ASC) rowNum 
+		FROM sys.columns c 
+		INNER JOIN sys.tables t 
+			ON		c.OBJECT_ID=t.OBJECT_ID 
+				AND t.name='tblHomeSpotterHistory_DT'
+				AND c.name NOT IN ('GoodToImport', 'ErrorDescription') 
+				AND t.schema_id = 9
+		INNER JOIN sys.types u 
+			ON c.user_type_id=u.user_type_id 
+		ORDER BY c.name ASC;
+SELECT * FROM @temp
+SELECT MAX(rowNum) FROM @temp; 
