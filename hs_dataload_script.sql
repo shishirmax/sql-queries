6a49d01306d1cf2b091de33cddb8af6cc509065a
Shishir <shishirmax@gmail.com>
2017-12-26 15:53:36 +0530

    data load script

--- hs_dataload_script.sql
+++ hs_dataload_script.sql
@@ -1,29 +1,32 @@
 --**** HomeSpotter History **************************************************************
---bcp homeSpotter.tblHomeSpotterHistory_bcp in D:\Edina\HomeSpotterFeed\HistoryData\contata_history-11-30-17.csv -S tcp:contata.database.windows.net -d Edina -U contata.admin@contata -P C@ntata123  -b 20000 -q -c -t","
+bcp homeSpotter.tblHomeSpotterHistory_bcp in D:\Edina\HomeSpotterFeed\HistoryData\contata_history-11-30-17.csv -S tcp:contata.database.windows.net -d Edina -U contata.admin@contata -P C@ntata123  -b 20000 -q -c -t","
 
 --EXEC homeSpotter.usp_InsertHomeSpotterHistory
 
 --EXEC homeSpotter.usp_MergeHomeSpotterHistory
 
---SELECT COUNT(1) FROM homeSpotter.tblHomeSpotterHistory_bcp
---SELECT COUNT(1) FROM homeSpotter.tblHomeSpotterHistory_FF
---SELECT COUNT(1) FROM homeSpotter.tblHomeSpotterHistory_DT
---SELECT COUNT(1) FROM homeSpotter.tblHomeSpotterHistory_AE
+SELECT COUNT(1) FROM homeSpotter.tblHomeSpotterHistory_bcp
+SELECT COUNT(1) FROM homeSpotter.tblHomeSpotterHistory_FF
+SELECT COUNT(1) FROM homeSpotter.tblHomeSpotterHistory_DT
+SELECT COUNT(1) FROM homeSpotter.tblHomeSpotterHistory_AE
+
+select * FROM homeSpotter.tblHomeSpotterHistory_AE
+truncate table homeSpotter.tblHomeSpotterHistory_bcp
 
 --**** HomeSpotter **************************************************************
 
-bcp homeSpotter.tblHomeSpotter_bcp in D:\Edina\HomeSpotterFeed\18Dec17\edina_contata_sessions.csv -S tcp:contata.database.windows.net -d Edina -U contata.admin@contata -P C@ntata123  -b 20000 -q -c -t","
+bcp homeSpotter.tblHomeSpotter_bcp in D:\Edina\HomeSpotterFeed\24Dec17\edina_contata_sessions.csv -S tcp:contata.database.windows.net -d Edina -U contata.admin@contata -P C@ntata123  -b 20000 -q -c -t","
 
 EXEC homeSpotter.usp_InsertHomeSpotter
 
 EXEC homeSpotter.usp_MergeHomeSpotter
 
-SELECT COUNT(1) As DimAgent FROM homeSpotter.DimAgent --2051
-SELECT COUNT(1) As DimDevice FROM homeSpotter.DimDevice --36735
-SELECT COUNT(1) As DimSession FROM homeSpotter.DimSession --442414
-SELECT COUNT(1) As DimUser FROM homeSpotter.DimUser --13416
-SELECT COUNT(1) As FactHomeSpotter FROM homeSpotter.FactHomeSpotter --444217
-SELECT COUNT(1) As FactHomeSpotterSummary FROM homeSpotter.FactHomeSpotterSummary --15555
+SELECT COUNT(1) As DimAgent					FROM homeSpotter.DimAgent --2055
+SELECT COUNT(1) As DimDevice				FROM homeSpotter.DimDevice --39944
+SELECT COUNT(1) As DimSession				FROM homeSpotter.DimSession --550497
+SELECT COUNT(1) As DimUser					FROM homeSpotter.DimUser --13485
+SELECT COUNT(1) As FactHomeSpotter			FROM homeSpotter.FactHomeSpotter --552302
+SELECT COUNT(1) As FactHomeSpotterSummary	FROM homeSpotter.FactHomeSpotterSummary --15643
 
 SELECT COUNT(1) As tblHomeSpotter_bcp FROM homeSpotter.tblHomeSpotter_bcp
 SELECT COUNT(1) As tblHomeSpotter_FF FROM homeSpotter.tblHomeSpotter_FF
