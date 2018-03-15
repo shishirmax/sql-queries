2f7b094914c2602aaf1c53d8c439bea043317a82
Shishir <shishirmax@gmail.com>
2018-03-15 10:07:54 +0530

    new analysis query added

--- Quickbook_Dump_Analysis.sql
+++ Quickbook_Dump_Analysis.sql
@@ -86,6 +86,12 @@ group by Num,FileName
 203036 NULL records avaialble in Num column
 
 Most of the available data is of Integer type.
+e.g:
+199268
+200003
+224501
+224721
+224888
 Few varchar data is also available which might needs a cleanup
 e.g:
 Med-2016
@@ -98,5 +104,114 @@ RecordCount	Num			FileName
 20			WRITE-OFF	2015-10.CSV
 
 The Amount column coresponding to these are in negative
+
+*/
+ select count(1),memo from zerorez.March_2018_Quickbook_Dump
+ where memo is not null
+ group by memo
+
+ /*
+ 367873 NULL records available on Memo column
+
+ Random data available 
+ e.g
+$120 topicals
+$120 training master bedroom oversized
+$120 training plus one
+$120 two area rugs
+$120 upper bedroom plus one
+$120 value
+- Lobby area, 3 Offices (1283 sq ft)
+ M2 Multisprayer
+ shipping
+!!!Call in twenty four hour?
+!/2 Flight
+!0% multi-svc discount
+
+Cleaning Required
+ */
+
+ select count(1),Name from zerorez.March_2018_Quickbook_Dump
+ group by Name
+ select count(distinct Name) from zerorez.March_2018_Quickbook_Dump
+
+ select * from zerorez.March_2018_Quickbook_Dump
+ where Name like '%[0-9]%'
+ /*
+ 203023 null records available for name column
+
+ -Heavy cleaning required on Name column 
+ -special character and numeric value is also available.
+ - where valid Name is available First and Last name is separated using comma
+
+ e.g:
+ 3515 2nd Ave S c/o YellowTree
+44th Street Dental
+18300 Minnetonka Blvd, LLC
+1300 On The Park Coop, Gayle Lamb
+27 LLC
+-, -
+.Jackson, Sue
+.Prestrud, Brandon
+?????, Ronita?
+
+Valid Names:
+Aamot, Brian
+Aamot, Jodi
+Aamot, Marge
+Aamot, Virginia
+Aamoth, Archie
+Aanden, Nicole
+
+
+ */
+
+select count(1),Item from zerorez.March_2018_Quickbook_Dump
+where Item is not null
+group by Item
+
+select count(distinct Item) from zerorez.March_2018_Quickbook_Dump
+
+/*
+203023 NULL records available in Item column
+336 Distinct Item Available
+records available with specific number and Item to be covered, few items has cost with them others dont.
+e.g(Items with cost)
+1. 100--$159 Three Room Special
+2. 100--$159 Two Room Special Wit
+3. 100--$169 Two Room Special With
+4. 1000--$117 Three Room Special
+5. 1001--$127 Two Rooms + Steps
+
+e.g(Items without cost)
+1. 100--VIP Three Room Special
+2. 102--Traffic Area Clean
+3. 103--Hallway
+4. 108--Bedroom
+*/
+
+select count(1) As RecordsCount,Item,[Item Description] from zerorez.March_2018_Quickbook_Dump
+where [Item Description] is null
+group by Item,[Item Description]
+order by 1 desc
+
+select count(distinct [Item Description]) from zerorez.March_2018_Quickbook_Dump
+where [Item Description] is not null and [Item Description] <> 'null'
+
+select count(distinct Item) from zerorez.March_2018_Quickbook_Dump
+
+/*
+553459 NULL records available for ItemDescription column
+77 Distinct Item Description available
+218 Items avaiable for which Item Description is not avialble
+Description of Items are listed under the column,
+For few Items there is null string is available.
+For many item there is no descriptions available.
+After most used Item(tax), Waste Disposal is the heighest Item used with 87558 count
+
+RecordsCount	Item												Item Description
+107158			Tax (Tax)											Tax
+87558			120 - Waste Disposal (120--Water and Disposal Fee)	120--Water and Disposal Fee
+15197			180--Free Home Service Inspect (null)				null
 
 */
