cc517a67262407233fae5d1df4017e332bd4c53f
Shishir <shishirmax@gmail.com>
2018-03-16 09:05:12 +0530

    new analysis query for calculating mean,median and sd

--- Quickbook_Dump_Analysis.sql
+++ Quickbook_Dump_Analysis.sql
@@ -1,22 +1,22 @@
 select 
 --count(*) 
 top 100 * 
-from zerorez.March_2018_Quickbook_Dump
+from zerorez.ZeroRezQuickbookDump_FF
 where 
 --[Type] IS NOT NULL and 
 NameType IS NOT NULL
 order by FileName 
 
-select top 100 * from zerorez.March_2018_Quickbook_Dump
+select top 100 * from zerorez.ZeroRezQuickbookDump_FF
 
-select count(*) from zerorez.March_2018_Quickbook_Dump
+select count(*) from zerorez.ZeroRezQuickbookDump_FF
 where [Type] IS NULL
 
 select count(1),NameType
-from zerorez.March_2018_Quickbook_Dump
+from zerorez.ZeroRezQuickbookDump_FF
 group by NameType
 
-select count(1) from zerorez.March_2018_Quickbook_Dump
+select count(1) from zerorez.ZeroRezQuickbookDump_FF
 where NameType IS NOT NULL
 
 /*
@@ -27,11 +27,11 @@ When value of nametype column is name only then all the corresponding column wil
 When value of nametype column is Total with Name then Qty and Amount column will consisit the toltal value of its Qty and Amount.
 */
 
-select count(1),Type from zerorez.March_2018_Quickbook_Dump
+select count(1),Type from zerorez.ZeroRezQuickbookDump_FF
 --where Type is null --203023
 group by Type
 
-select top 10 * from zerorez.March_2018_Quickbook_Dump
+select top 10 * from zerorez.ZeroRezQuickbookDump_FF
 where 
 --Type = 'Credit Memo' 
 Type = 'Invoice'
@@ -46,16 +46,16 @@ When Type = Credit Memo(Qty= -2, Amount= -80.00, Balance= -239.00)
 When Type = Invoice(Qty = 1, Amount= 13.11, Balance= 197.11)
 */
 
-select count(1),MONTH(CAST([Date] as DATE)),YEAR(CAST([Date] as DATE)) from zerorez.March_2018_Quickbook_Dump
+select count(1),MONTH(CAST([Date] as DATE)),YEAR(CAST([Date] as DATE)) from zerorez.ZeroRezQuickbookDump_FF
 group by MONTH(CAST([Date] as DATE)),YEAR(CAST([Date] as DATE))
 order by YEAR(CAST([Date] as DATE)),MONTH(CAST([Date] as DATE))
 
-SELECT COUNT(1) As RecordCount,YEAR(CAST([Date] AS DATE)) As DateYear FROM zerorez.March_2018_Quickbook_Dump
+SELECT COUNT(1) As RecordCount,YEAR(CAST([Date] AS DATE)) As DateYear FROM zerorez.ZeroRezQuickbookDump_FF
 GROUP BY YEAR(CAST([Date] AS DATE))
 ORDER BY YEAR(CAST([Date] AS DATE))
 
 
-select count(distinct date) from zerorez.March_2018_Quickbook_Dump
+select count(distinct date) from zerorez.ZeroRezQuickbookDump_FF
 where date is not null
 /*
 203023 NULL records available in Date column
@@ -72,11 +72,11 @@ RecordCount	DateYear
 
 */
 
-select count(1),Num,FileName from zerorez.March_2018_Quickbook_Dump
+select count(1),Num,FileName from zerorez.ZeroRezQuickbookDump_FF
 where Num is NOT NULL
 group by Num,FileName
 
-select count(1) As RecordCount,Num,FileName from zerorez.March_2018_Quickbook_Dump
+select count(1) As RecordCount,Num,FileName from zerorez.ZeroRezQuickbookDump_FF
 where Num is NOT NULL
 --and Num not like '%[0-9]%'
 and Num like '%[^a-z,A-Z,0-9]%'
@@ -106,7 +106,7 @@ RecordCount	Num			FileName
 The Amount column coresponding to these are in negative
 
 */
- select count(1),memo from zerorez.March_2018_Quickbook_Dump
+ select count(1),memo from zerorez.ZeroRezQuickbookDump_FF
  where memo is not null
  group by memo
 
@@ -131,11 +131,11 @@ $120 value
 Cleaning Required
  */
 
- select count(1),Name from zerorez.March_2018_Quickbook_Dump
+ select count(1),Name from zerorez.ZeroRezQuickbookDump_FF
  group by Name
- select count(distinct Name) from zerorez.March_2018_Quickbook_Dump
+ select count(distinct Name) from zerorez.ZeroRezQuickbookDump_FF
 
- select * from zerorez.March_2018_Quickbook_Dump
+ select * from zerorez.ZeroRezQuickbookDump_FF
  where Name like '%[0-9]%'
  /*
  203023 null records available for name column
@@ -166,11 +166,11 @@ Aanden, Nicole
 
  */
 
-select count(1),Item from zerorez.March_2018_Quickbook_Dump
+select count(1),Item from zerorez.ZeroRezQuickbookDump_FF
 where Item is not null
 group by Item
 
-select count(distinct Item) from zerorez.March_2018_Quickbook_Dump
+select count(distinct Item) from zerorez.ZeroRezQuickbookDump_FF
 
 /*
 203023 NULL records available in Item column
@@ -190,15 +190,15 @@ e.g(Items without cost)
 4. 108--Bedroom
 */
 
-select count(1) As RecordsCount,Item,[Item Description] from zerorez.March_2018_Quickbook_Dump
+select count(1) As RecordsCount,Item,[Item Description] from zerorez.ZeroRezQuickbookDump_FF
 where [Item Description] is null
 group by Item,[Item Description]
 order by 1 desc
 
-select count(distinct [Item Description]) from zerorez.March_2018_Quickbook_Dump
+select count(distinct [Item Description]) from zerorez.ZeroRezQuickbookDump_FF
 where [Item Description] is not null and [Item Description] <> 'null'
 
-select count(distinct Item) from zerorez.March_2018_Quickbook_Dump
+select count(distinct Item) from zerorez.ZeroRezQuickbookDump_FF
 
 /*
 553459 NULL records available for ItemDescription column
@@ -214,4 +214,178 @@ RecordsCount	Item												Item Description
 87558			120 - Waste Disposal (120--Water and Disposal Fee)	120--Water and Disposal Fee
 15197			180--Free Home Service Inspect (null)				null
 
+*/
+
+select top 100 * from zerorez.ZeroRezQuickbookDump_FF
+where Qty IS NULL and Type IS NOT NULL
+
+/*
+101629 NULL records available for Qty column
+
+Qty column is mostly NULL in those places where Adjustments are made in the Invoices for the customer,
+There Respective Item and Item description is also like Adjustment.
+
+e.g:
+
+Num: 363616
+Memo: Invoice Adjustments
+Name: Campbell, Melissa
+Item: Adjustment - Residential (Invoice Adjustments)
+Item Description: Invoice Adjustments
+Qty: NULL
+Sales Price: -0.40
+Amount: -0.40
+Balance: 426.55
+FileName: 2017-08.CSV
+*/
+
+select count(1),CAST([Sales Price] as money) from zerorez.ZeroRezQuickbookDump_FF
+--where [Sales Price] is not null
+group by CAST([Sales Price] as money)
+order by 1 desc
+
+select MIN(CAST([SalesPrice] as money)) from zerorez.ZeroRezQuickbookDump_FF -- -2550.00
+select MAX(CAST([SalesPrice] as money)) from zerorez.ZeroRezQuickbookDump_FF -- 16643.31
+
+select avg(cast([SalesPrice] as money)) from zerorez.ZeroRezQuickbookDump_FF -- MEAN: 40.9117
+
+select 
+((Select Top 1 [SalesPrice]
+From (
+Select Top 50 Percent CAST([SalesPrice] as money) as [SalesPrice]
+From	ZeroRez.ZeroRezQuickbookDump_FF
+Where	CAST([SalesPrice] as money) Is NOT NULL
+Order By CAST([SalesPrice] as money)
+) As A
+Order By CAST([SalesPrice] as money) DESC)
++
+(Select Top 1 [SalesPrice]
+From (
+Select Top 50 Percent CAST([SalesPrice] as money) as [SalesPrice]
+From	ZeroRez.ZeroRezQuickbookDump_FF
+Where	CAST([SalesPrice] as money) Is NOT NULL
+Order By CAST([SalesPrice] as money) DESC
+) As A
+Order By CAST([SalesPrice] as money) Asc))/2 -- MEDIAN: 19.13
+
+SELECT TOP 1 with ties cast([SalesPrice] as money)
+FROM ZeroRez.ZeroRezQuickbookDump_FF
+WHERE cast([SalesPrice] as money) IS Not NULL
+GROUP BY cast([SalesPrice] as money)
+ORDER BY COUNT(*) DESC --1 MODE
+
+SELECT STDEV(cast([SalesPrice] as money)) 
+FROM ZeroRez.ZeroRezQuickbookDump_FF -- SD: 114.657210109228
+
+select count(distinct [Sales Price]) from ZeroRez.ZeroRezQuickbookDump_FF 
+/*
+203037 NULL records available
+Distinct Records: 10593
+Minimum value: -2550.00
+Maximum value: 16643.31
+Mean: 40.9117
+Median: 19.13
+Standard Deviation: 114.657210109228
+
+*/
+
+select count(1),CAST(Amount as money) from zerorez.ZeroRezQuickbookDump_FF
+group by CAST(Amount as money)
+order by 1 desc
+
+select count(Distinct CAST(Amount as money))from zerorez.ZeroRezQuickbookDump_FF
+
+select MIN(CAST(Amount as money)) from zerorez.ZeroRezQuickbookDump_FF -- Min: -2550.00
+select MAX(CAST(Amount as money)) from zerorez.ZeroRezQuickbookDump_FF -- Max: 16643.31
+
+select avg(cast(Amount as money)) from zerorez.ZeroRezQuickbookDump_FF -- MEAN: 50.7353
+
+select 
+((Select Top 1 Amount
+From (
+Select Top 50 Percent CAST(Amount as money) as Amount
+From	ZeroRez.ZeroRezQuickbookDump_FF
+Where	CAST(Amount as money) Is NOT NULL
+Order By CAST(Amount as money)
+) As A
+Order By CAST(Amount as money) DESC)
++
+(Select Top 1 Amount
+From (
+Select Top 50 Percent CAST(Amount as money) as Amount
+From	ZeroRez.ZeroRezQuickbookDump_FF
+Where	CAST(Amount as money) Is NOT NULL
+Order By CAST(Amount as money) DESC
+) As A
+Order By CAST(Amount as money) Asc))/2 -- MEDIAN: 20.00
+
+SELECT TOP 1 with ties cast(Amount as money)
+FROM ZeroRez.ZeroRezQuickbookDump_FF
+WHERE cast(Amount as money) IS Not NULL
+GROUP BY cast(Amount as money)
+ORDER BY COUNT(*) DESC --1 MODE
+
+SELECT STDEV(cast(Amount as money)) 
+FROM ZeroRez.ZeroRezQuickbookDump_FF --SD: 127.875503860436
+
+/*
+101501 NULL Records available
+Distinct records: 31777
+Min: -2550.00
+Max: 16643.31
+Median: 20.00
+StandardDeviation: 127.875503860436
+MEAN: 50.7353
+*/
+
+select  * from zerorez.ZeroRezQuickbookDump_FF
+
+select count(1),CAST(Balance as money) from zerorez.ZeroRezQuickbookDump_FF
+group by CAST(Balance as money)
+order by 1 desc
+
+select count(Distinct CAST(Balance as money))from zerorez.ZeroRezQuickbookDump_FF
+
+select MIN(CAST(Balance as money)) from zerorez.ZeroRezQuickbookDump_FF -- Min: -2699.46
+select MAX(CAST(Balance as money)) from zerorez.ZeroRezQuickbookDump_FF -- Max: 25893.22
+
+select avg(cast(Balance as money)) from zerorez.ZeroRezQuickbookDump_FF -- MEAN: 247.4773
+
+select 
+((Select Top 1 Balance
+From (
+Select Top 50 Percent CAST(Balance as money) as Balance
+From	ZeroRez.ZeroRezQuickbookDump_FF
+Where	CAST(Balance as money) Is NOT NULL
+Order By CAST(Balance as money)
+) As A
+Order By CAST(Balance as money) DESC)
++
+(Select Top 1 Balance
+From (
+Select Top 50 Percent CAST(Balance as money) as Balance
+From	ZeroRez.ZeroRezQuickbookDump_FF
+Where	CAST(Balance as money) Is NOT NULL
+Order By CAST(Balance as money) DESC
+) As A
+Order By CAST(Balance as money) Asc))/2 -- MEDIAN: 179.00
+
+SELECT TOP 1 with ties cast(Balance as money)
+FROM ZeroRez.ZeroRezQuickbookDump_FF
+WHERE cast(Balance as money) IS Not NULL
+GROUP BY cast(Balance as money)
+ORDER BY COUNT(*) DESC --1 MODE
+
+SELECT STDEV(cast(Balance as money)) 
+FROM ZeroRez.ZeroRezQuickbookDump_FF --SD: 404.553767873828
+
+
+/*
+101500 NULL Records available
+Distinct Records: 50646
+Min: -2699.46
+Max: 25893.22
+MEDIAN: 179.00
+MEAN: 247.4773
+SD: 404.553767873828
 */
