31305448aab6464f3a0796a7b70e874d3e07ad7a
Shishir <shishirmax@gmail.com>
2018-06-28 17:48:24 +0530

    added stored procedure for insert

--- subscription_Schema/SchemaQuery.sql
+++ subscription_Schema/SchemaQuery.sql
@@ -47,15 +47,78 @@ SELECT * FROM LogError
 DROP TABLE LogError
 
 --Creating a StoredProcedure for inserting data into subscriptionStatus
+ALTER PROCEDURE sp_InsertIntoSubscriptionStatus
+AS
+BEGIN
+	DECLARE 
+		@modifiedDate DATETIME
+		,@ErrorCount INT
+	SET @modifiedDate = GETDATE()
 
+	BEGIN TRY
+		BEGIN TRAN
+			INSERT INTO dbo.subscriptionStatus(
+				CustomerID
+				,TransactionID
+				,TransactionStatus
+				,TransactionReference
+				,TransacionTimeStamp
+				,ModifiedDate
+				)
+			SELECT
+				CustomerID
+				,TransactionID
+				,TransactionStatus
+				,TransactionReference
+				,TransacionTimeStamp
+				,@modifiedDate
+			FROM dbo.StagingSubscriptionStatus
+
+			SET @ErrorCount = @@ROWCOUNT
+			
+			TRUNCATE TABLE dbo.StagingSubscriptionStatus
+		COMMIT TRAN
+	END TRY
+
+	BEGIN CATCH
+		
+		IF @@TRANCOUNT>0
+		ROLLBACK TRAN
+
+		INSERT INTO [LogError] (
+			objectName          
+			,ErrorCode          
+			,ErrorDescription          
+			,ErrorGenerationTime)          
+			SELECT          
+			OBJECT_NAME(@@PROCID),          
+			ERROR_NUMBER() AS ErrorCode,          
+			'Error of Severity: ' + CAST(ERROR_SEVERITY() AS varchar(4))          
+			+ ' and State: ' + CAST(ERROR_STATE() AS varchar(8))          
+			+ ' occured in Line: ' + CAST(ERROR_LINE() AS varchar(10))          
+			+ ' with following Message: ' + ERROR_MESSAGE() AS ErrorColumnDescription,          
+			GETDATE()
+	END CATCH
+END
+	
+--Executing Stored Procedure to insert data into subscriptionStatus table
+EXEC sp_InsertIntoSubscriptionStatus
+
+SP_HELPTEXT 'sp_InsertIntoSubscriptionStatus'
 
 
 
 
 SELECT * FROM StagingSubscriptionStatus
-TRUNCATE TABLE StagingSubscriptionStatus
-SELECT * FROM subscriptionStatus
+--TRUNCATE TABLE StagingSubscriptionStatus
 
+SELECT * FROM subscriptionStatus
+--TRUNCATE TABLE subscriptionStatus
+
+--Alter table subscriptionStatus to add column ModifiedDate
+
+ALTER TABLE subscriptionStatus
+ADD ModifiedDate DATETIME
 SELECT CAST('"02-June-2018"' AS DATETIME)
 
 --Insert Data using BULK INSERT SCRIPT
