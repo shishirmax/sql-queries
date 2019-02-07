f017496aeeba8bebf891d1e4bf2168fecdede6da
Shishir <shishirmax@gmail.com>
2019-02-07 09:36:15 +0530

    query updated

--- Q&A/UpsertQuery.sql
+++ Q&A/UpsertQuery.sql
@@ -26,7 +26,7 @@ SELECT ROUND(AVG(Salary) - AVG(CAST(REPLACE(Salary,'0','') AS INT)),1) FROM hEmp
 
 
 SELECT * FROM hEmployees
-EXEC InsertOrUpdateEmployee 'Kimberly',6500
+EXEC InsertOrUpdateEmployee 'Preeti',26500
 
 SELECT * FROM hEmployees
 WHERE UpdateDate IS NULL
